cf690cb8bcdfc1054639de3febc72abbb85611b7
lesleymj <leslyval0782@gmail.com>
2015-07-28 16:32:44 -0500

    lesley

---
+++ js/galeria.js
@@ -0,0 +1,14 @@
+function agregarImagen(imagen)
+{
+document.getElementById("imagenprincipal").src=imagen;
+}
+function rosas(imagen)
+{
+document.getElementById("imagenprincipal").src=imagen;
+}
+
+function flores(imagen)
+{
+var imagen = imagen.src;
+agregarImagen(imagen);
+}
